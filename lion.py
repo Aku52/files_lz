63b9a1593492f261d381f4513427651869295e92
Aku52 <kotik.52@tutamail.com>
2024-11-29 22:54:13 +0300

    files_1_1

--- lion.py
+++ lion.py
@@ -1,50 +1,74 @@
 import docx
 from collections import Counter
-import re # модуль для работы с текстом 
+import re # Модуль для работы с текстом 
 import pandas as pd
     
 doc = docx.Document('lion.docx')
 
-quantity_par = len(doc.paragraphs) # количество обзацев
-no_repet_list = [] #список для слов без повторений 
+quantity_par = len(doc.paragraphs) # Количество обзацев
+no_repet_list = [] # Cписок для слов без повторений 
 
 general_list = []
-rus_let = re.compile(r'[а-яё]', re.IGNORECASE) #регулярное выражение для поиска русских букв
+rus_let = re.compile(r'[а-яё]', re.IGNORECASE) # Регулярное выражение для поиска русских букв
 
-rus_text = [] #список для всего текста (только русские слова)
-rus_word = re.compile(r'\b[а-яё]+\b',re.IGNORECASE) #регулярное выражение для поиска русских слов 
+rus_text = [] # Список для всего русского текста 
+rus_word = re.compile(r'\b[а-яё]+\b',re.IGNORECASE) # Выражение для поиска русских слов 
 
-for paragraph in doc.paragraphs: #проходимся по всем параграфам в документе
-    text = paragraph.text.lower() # приводим текст к нижнему регистру
-    words = rus_word.findall(text) # находим все русские слова
-    letters = rus_let.findall(text) # находим все русские буквы
-    general_list .extend(letters) #добовляем буквы в общий список
+for paragraph in doc.paragraphs: # Проходимся по всем параграфам в документе
+    text = paragraph.text.lower() # Приводим текст к нижнему регистру
+    words = rus_word.findall(text) # Находим все русские слова
+    letters = rus_let.findall(text) # Находим все русские буквы
+    general_list .append(letters) # Добовляем буквы в общий список
 
 
     for word in words:
         rus_text.append(word)
-        if word not in no_repet_list: # проверка на на наличие слова в списке 
-            no_repet_list.append(word) #добавления слова 
+        if word not in no_repet_list: # Проверка на на наличие слова в списке 
+            no_repet_list.append(word) # Добавления слова 
 
-word_counts = Counter(rus_text) #подсчитываем частоту встречаемости слов
-for word, count in word_counts.items():
-    data_word = {f'{word}: {count}'}
 
-letters_counts = Counter(general_list) #подсчитываем частоту встречаемости слов
-df = pd.DataFrame(letters_counts.items(),columns=['Letter','Frequency'])
-print(df)
+# Cлова в тексте
+rus_text_len = len(rus_text) # Количество слов в списке
 
-#Закидываю результат в новый doc файл
-doc_letter = docx.Document() #создаю новый документ
+word_counts = Counter(rus_text) # Подсчитываем частоту встречаемости слов
 
-doc_letter.add_heading('Встречаемость букв в тексте', level=1)#добовляем заголовок
+print_word = {
+    'Слово': list(word_counts.keys()),
+    'Частота встречи,раз': list(word_counts.values()),
+    'Частота встречи в %': [round((count / rus_text_len) * 100, 2) for count in word_counts.values()]
+}
 
-#Добавление данных DataFrame в документ как текст
-for index, row in df.iterrows():
-    letter_text = [f"{col}: {row[col]}" for col in df.columns]
-    doc_letter.add_paragraph(letter_text)
+df_word = pd.DataFrame(print_word)
 
-doc_letter.save('встречаемость букв.docx') # Сохраняем документ
+dict_word = docx.Document() # Создаем новый документ
 
-print(len(no_repet_list))
-print(general_list)
+dict_word.add_heading('Встречаемость слов в тексте', 0) # Добовляем заголовок
+
+table = dict_word.add_table(rows =1, cols =3) # Создаем таблицу с одной строкой и тремя колонками 
+hdr_cells = table.rows[0].cells # cells - доступа к конкретным ячейкам строки, чтобы заполнить их содержимым
+hdr_cells[0].text = 'Слово'
+hdr_cells[1].text = 'Частота встречи,раз'
+hdr_cells[2].text = 'Частота встречи в %'
+
+for i in range(len(df_word)):
+    row_cells = table.add_row().cells # Добавляем новой строки в таблицу                                   (i name age)
+    row_cells[0].text = df_word.loc[i, 'Слово'] #loc - выбор строки с индексом [i], в DataFrame это по типу (0 bob 14 ) 
+    row_cells[1].text = str(df_word.loc[i, 'Частота встречи,раз'])
+    row_cells[2].text = str(df_word.loc[i, 'Частота встречи в %'])
+
+dict_word.save('встречаемость_слов.docx') # Сохраняем документ
+
+
+# Буквы в тексте
+
+letters_counts = Counter(letters) # Cчитаем частоту встречаемости букв
+df_letter = pd.DataFrame(letters_counts.items(),columns=['Буквы','Количество'])
+df_letter['Количество'] = pd.to_numeric(df_letter['Количество']) # to_numeric преобразования значений в столбцах DataFrame в числовой тип данных
+
+plt.figure(figsize=(10, 6))# Ширина фигуры будет 10 дюймов, а высота — 6 дюймов.
+plt.bar(df_letter['Буквы'], df_letter['Количество'], color='skyblue')# plt.bar- создаем столбчатую диаграмму.
+plt.xlabel('Буквы')# оси X, представляющие буквы
+plt.ylabel('Количество')# оси Y, представляющие Количество
+plt.title('Встречаемость букв в тексте')# plt.title - задаем заголовок для графика.
+plt.grid(True)# plt.grid - включаем отображение сетки на графике, что помогает лучше визуализировать данные.
+plt.show()
--- titanic.py
+++ titanic.py
@@ -1,48 +1,28 @@
 import pyarrow.parquet as pq
 
-table = pq.read_table("titanic.parquet")
-df = table.to_pandas()
-df
-
-'''import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Функция для чтения данных, сохранения в CSV и построения гистограммы
-def process_titanic_data(parquet_file, output_csv):
-    # Чтение данных из parquet файла
-    data = pd.read_parquet(parquet_file)
+df = pd.read_parquet('titanic.parquet') # Чтение файла 
 
-    # Сохранение данных в формат CSV
-    data.to_csv(output_csv, index=False, encoding="utf-8-sig")
-    print(f"Данные успешно сохранены в файл: {output_csv}")
+df.to_csv('titanic.parquet', index=False) # Cохранение данный DataFrame в csv и убираем индексы 
 
-    # Проверка структуры данных
-    print("Первые строки данных:")
-    print(data.head())
+df_csv = pd.read_csv('titanic.parquet') # Читаем csv файл
 
-    # Построение гистограммы
-    plot_survival_by_class(data)
+quantity_passengers = df_csv.groupby('Pclass').size() # Мы группируем DataFrame по столбцу Pclass пассажира.size(), функция возвращает количество элементов в каждой группе.
+survived = df_csv[df_csv['Survived'] == 1].groupby('Pclass').size() / quantity_passengers * 100 # Выжившие пассажиры: количество, процент 
+not_survived = df_csv[df_csv['Survived'] == 0].groupby('Pclass').size() / quantity_passengers * 100 # Не выжившие пассажиры: количество, процент 
 
-# Функция для построения гистограммы
-def plot_survival_by_class(data):
-    # Группировка данных по классу билета и статусу выживаемости
-    survival_data = data.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
+survival_data = pd.DataFrame({
+    'Выжившие': survived,
+    'Не выжившие': not_survived
+})
 
-    # Нормализация данных для отображения в процентах
-    survival_percent = survival_data.div(survival_data.sum(axis=1), axis=0) * 100
-
-    # Построение графика
-    survival_percent.plot(kind='bar', stacked=True, color=["skyblue", "lightcoral"], figsize=(10, 6))
-    plt.title("Выживаемость пассажиров Титаника", fontsize=14)
-    plt.xlabel("Класс билета", fontsize=12)
-    plt.ylabel("Процент пассажиров", fontsize=12)
-    plt.legend(["Выжил", "Не выжил"], loc="upper left", fontsize=10)
-    plt.grid(axis="y", linestyle="--", alpha=0.7)
-    plt.xticks(ticks=range(len(survival_percent.index)), labels=["Первый класс", "Второй класс", "Третий класс"], rotation=0)
-    plt.show()
-
-# Основной код
-parquet_file = "titanic.parquet"  # Имя исходного parquet файла
-output_csv = "titanic.csv"       # Имя файла для сохранения в формате CSV
-
-process_titanic_data(parquet_file, output_csv)'''
+# Построение гистограммы
+ax = survival_data.plot(kind='bar', stacked=True, color=['green', 'red']) # kind - тип графика bar - столбчатая диаграмма
+                                                                          # stacked - столбцы будут накладываться друг на друга
+ax.set_xlabel('Класс билета') #название оси X
+ax.set_ylabel('Доля пассажиров (%)') #название оси Y
+ax.set_title('Выживаемость пассажиров в зависимости от класса билета') #заголовок для графика
+ax.legend(title='Статус')
+plt.show()
